c0e6cbe98fde6154dd296e2341e1a38b6652632b
Isaac0312 <isaacejarque@hotmail.com>
2022-10-25 09:32:17 +0200

    Update ConnectDB.java

--- TA18/src/Connection/ConnectDB.java
+++ TA18/src/Connection/ConnectDB.java
@@ -1,5 +1,28 @@
 package Connection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
 
+import javax.swing.JOptionPane;
+
+import com.sun.istack.internal.logging.Logger;
+
+import java.sql.DriverManager;
 public class ConnectDB {
-
+	private Connection conexion;
+	
+	
+	public void create_connection() {
+		
+	}
+	
+	public void closeConnection() {
+		try {
+			conexion.close();
+			JOptionPane.showMessageDialog(null, "Connection is closed with the server");
+		} catch (SQLException ex) {
+			Logger.getLogger(ConnectDB.class.getName(), null).log(Level.SEVERE,null,ex);
+		}
+	}
+	
 }
